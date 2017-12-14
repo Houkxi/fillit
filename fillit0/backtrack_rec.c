ae05649bb1b0419b3900a4190bb1d301e02e4ccb
Mathieu MANLEY <mmanley@e2r4p4.42.fr>
2017-12-14 14:48:57 +0100

    Retouch BK

--- fillit0/backtrack_rec.c
+++ fillit0/backtrack_rec.c
@@ -6,7 +6,7 @@
 /*   By: mmanley <mmanley@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 15:12:48 by mmanley           #+#    #+#             */
-/*   Updated: 2017/12/13 18:45:08 by mmanley          ###   ########.fr       */
+/*   Updated: 2017/12/14 14:48:32 by mmanley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,47 +30,22 @@ char	**backy_tracky(char **newtab, t_forme **ptr, int position, int size)
 
 	co->x = position % size;
 	co->y = position / size;
-	//printf("\n%d\n", size);
-	//printf("Size %d, Pos %d, y %d, x %d\n", size, position, co->y, co->x);
-	//printf("%d  DOT (%d, %d)\n\n", size, is_size_good((*ptr)->next->tab, size, newtab), check_posabilty(*ptr, *co, (*ptr)->next->tab, newtab));
-
-	if ((is_size_good((*ptr)->next->tab, size, newtab)) == 0)
+	printf("\n%d\n", size);
+	//printf("Size %d, Pos %d, y %d, x %d----------", size, position, co->y, co->x);
+	//printf("%d  Possible (%d, %d)\n\n", size, is_size_good((*ptr)->next->tab, size, newtab), check_posabilty(*ptr, *co, (*ptr)->next->tab, newtab));
+	if ((check_posabilty(*ptr, *co, (*ptr)->next->tab, newtab)) == 0)
 	{
-		printf("WRONG SIZE\n");
-		free(newtab);
-		//newtab = ft_newtab(size + 1, size + 1, '.');
-		return (NULL);
-	}
-	///////WHILE POSSIBILITY CHECK ALL OUTCOMES //////////
-	if ((check_posabilty(*ptr, *co, (*ptr)->next->tab, newtab)) == 0
-			&& co->y < size - 1)
-	{
-
-		/////////////PROBLEME DE CONDITION, change pos au lieu de TAILLE////////
-		//printf("\nSize %d, Pos %d, y %d, x %d\n\n", size, position, co->y, co->x);
-		//printf("WRONG POS   \n");
-		//printf("(%d, %d), POS = %d\n", co->y, co->x, position);
-		// PROBLEME, N'est pas capable de reagrandir//
+		if (co->y == size - 1 && co->x == size - 1)
+		{
+			printf("WRONG SIZE\n");
+			free(newtab);
+			return (NULL);
+		}
+		printf("WRONG POS   \n");
 		return (backy_tracky(newtab, ptr, position + 1, size));
 	}
-
-	////////////
-	///////////	IF THE NEXT TETRI CAN'T FIT MOVE THIS ONE //////////////
-	//////////
-
-	if ((check_posabilty(*ptr, *co, (*ptr)->next->tab, newtab)) == 1
-			&& co->y < size - 1 &&
-		(check_posabilty(*ptr, *co, (*ptr)->next->next->tab, newtab)) == 0)
-	{
-		printf("NEW POS\n");
-		return (backy_tracky(newtab, ptr, position + 1, size));
-	}
-/**	if (newtab[co->y] && co->y < size - 1 && newtab[co->y][co->x] != '.')
-	{
-		printf("Incr\n" );
-		return (backy_tracky(newtab, ptr, position + 1, size));
-	}*/
-	else if ((pos_tetriminos(*ptr, newtab, *co)) == 0 && co->y < size - 1)
+	printf("Good\n");
+	if ((pos_tetriminos(*ptr, newtab, *co)) == 0 && co->y < size - 1)
 	{
 		printf("Rater COPY\n");
 		return (NULL);
